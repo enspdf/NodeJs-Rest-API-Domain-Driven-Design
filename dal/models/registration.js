90200c91a7f7f5c8513b43297e69a1ab5230baf9
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-05-02 12:24:12 -0500

    database models / migrations

---
+++ dal/models/registration.js
@@ -0,0 +1,16 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Registration = sequelize.define(
+    "Registration",
+    {
+      sectionId: DataTypes.INTEGER,
+      studentId: DataTypes.INTEGER,
+      status: DataTypes.STRING,
+    },
+    {}
+  );
+  Registration.associate = function (models) {
+    // associations can be defined here
+  };
+  return Registration;
+};
